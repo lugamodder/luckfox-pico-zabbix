b61ac9bb11d3e5f589476fc232e95122a898b8d5
lugamodder <25221685+lugamodder@users.noreply.github.com>
2024-06-19 09:41:23 +0300

    Update rootfs.sh

--- rootfs.sh
+++ rootfs.sh
@@ -26,7 +26,7 @@ rootfs_workspace_new
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Create docker
-$DOCKER_CONTAINER="arm32v7/alpine:3.18.6"
+DOCKER_CONTAINER="arm32v7/alpine:3.18.6"
 docker container rm -f armv7alpine
 docker pull $DOCKER_CONTAINER
 docker run \
