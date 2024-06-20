9c56023394eed1a2bb05fe33b5294b2edc5e092e
lugamodder <25221685+lugamodder@users.noreply.github.com>
2024-06-20 17:57:46 +0300

    Update rootfs.sh

--- rootfs.sh
+++ rootfs.sh
@@ -61,10 +61,7 @@ overlay() {
 
   ln -s "/etc/init.d/10_usb_gadget" \
     "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/10_usb_gadget"
-    
- # ln -s "/etc/init.d/20_symlink" \
- #   "$ROOTFS_WORKSPACE_MNT/etc/runlevels/boot/20_symlink"
-    
+      
   ln -s "$ROOTFS_WORKSPACE_MNT/run/" "$ROOTFS_WORKSPACE_MNT/var/run/"
   
 }
